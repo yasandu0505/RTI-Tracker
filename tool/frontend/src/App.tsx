4e891483863a9350fce271f4f5eb9ecc9175595a
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2026-04-28 12:45:20 +0530

    feat : protect routes

--- tool/frontend/src/App.tsx
+++ tool/frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import { SignedIn, SignOutButton } from '@asgardeo/react'
+import { ProtectedRoute } from '@asgardeo/react-router';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { Layout } from './components/Layout';
+import { LoginRedirect } from './components/LoginRedirect';
+
 import { Templates } from './pages/Templates';
 import { Receivers } from './pages/Receivers';
 
@@ -7,10 +13,20 @@ export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="templates" element={<Templates />} />
-          <Route path="receivers" element={<Receivers />} />
+        <Route path="/" element={
+          <ProtectedRoute redirectTo="/signin">
+            <Layout />
+          </ProtectedRoute>
+        }>
+          <Route path="templates" element={
+            <ProtectedRoute redirectTo="/signin">
+              <Templates /></ProtectedRoute>} />
+
+          <Route path="receivers" element={
+            <ProtectedRoute redirectTo="/signin">
+              <Receivers /></ProtectedRoute>} />
         </Route>
+        <Route path="/signin" element={<LoginRedirect />} />
       </Routes>
     </BrowserRouter>
   );
