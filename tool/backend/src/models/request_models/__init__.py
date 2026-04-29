2f29e32a18a94e3b7a49dde093ee5343c1e3fd46
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2026-04-29 17:06:35 +0530

    resolve merge conflicts

--- tool/backend/src/models/request_models/__init__.py
+++ tool/backend/src/models/request_models/__init__.py
@@ -2,12 +2,14 @@ from .senders import SenderRequest
 from .rti_templates import RTITemplateRequest
 from .institutions import InstitutionRequest
 from .receiver import ReceiverRequest, ReceiverUpdateRequest
+from .positions import PositionRequest
 
 __all__ = [
     "SenderRequest",
     "RTITemplateRequest",
     "InstitutionRequest",
     "ReceiverRequest",
-    "ReceiverUpdateRequest"
+    "ReceiverUpdateRequest",
+    "PositionRequest"
 ]
 
