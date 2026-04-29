8a0b4bf8de31d290d16b2b7ea0095fad7a52d2b4
ChanukaUOJ <chamithroxmpc@gmail.com>
2026-04-29 08:40:30 +0530

    auth enabled in routers

--- tool/backend/src/routers/institution_router.py
+++ tool/backend/src/routers/institution_router.py
@@ -20,7 +20,7 @@ def get_institutions_endpoint(
     page: int = Query(1, ge=1, description="page number"),
     page_size: int = Query(10, ge=1, le=100, description="page size"),
     service: InstitutionService = Depends(get_institution_service),
-    # user: User = Depends(RoleChecker([UserRole.ADMIN, UserRole.USER]))
+    user: User = Depends(RoleChecker([UserRole.ADMIN, UserRole.USER]))
     ):
     response = service.get_institutions(page=page, page_size=page_size)
     return response
