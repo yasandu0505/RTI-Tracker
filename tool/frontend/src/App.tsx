857120f03852d5ad3ab25ac00675a3a42079489d
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2026-05-05 11:40:14 +0530

    feat: loading fix in authentication flow

--- tool/frontend/src/App.tsx
+++ tool/frontend/src/App.tsx
@@ -4,18 +4,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Layout } from './components/Layout';
 import { LoginRedirect } from './components/LoginRedirect';
+import { PreLoader } from './components/PreLoader';
 
 import { Templates } from './pages/Templates';
 import { Receivers } from './pages/Receivers';
+import { useAsgardeo } from '@asgardeo/react';
 
 export function App() {
+  const { isLoading, isSignedIn } = useAsgardeo();
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={
-          <ProtectedRoute redirectTo="/signin">
-            <Layout />
-          </ProtectedRoute>
+          isLoading && isSignedIn ? (
+            <PreLoader message="Authentication in progress..." />
+          ) : (
+            <ProtectedRoute redirectTo="/signin">
+              <Layout />
+            </ProtectedRoute>
+          )
         }>
           <Route path="templates" element={<Templates />} />
           <Route path="receivers" element={<Receivers />} />
