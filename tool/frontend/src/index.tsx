f7baf96d5533e01dc4dff76c32129f12c3563fc0
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2026-05-08 16:21:44 +0530

    config mounted

--- tool/frontend/src/index.tsx
+++ tool/frontend/src/index.tsx
@@ -4,12 +4,22 @@ import { App } from "./App";
 import { AsgardeoProvider } from '@asgardeo/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const ASGARDEO_CLIENT_ID = import.meta.env.VITE_ASGARDEO_CLIENT_ID;
-const ASGARDEO_BASE_URL = import.meta.env.VITE_ASGARDEO_BASE_URL;
+// const ASGARDEO_CLIENT_ID = import.meta.env.VITE_ASGARDEO_CLIENT_ID;
+// const ASGARDEO_BASE_URL = import.meta.env.VITE_ASGARDEO_BASE_URL;
 
-if (!ASGARDEO_CLIENT_ID || !ASGARDEO_BASE_URL) {
-    console.error("Missing Asgardeo configuration. Please check your .env file.");
-}
+
+
+// if (!ASGARDEO_CLIENT_ID || !ASGARDEO_BASE_URL) {
+//     console.error("Missing Asgardeo configuration. Please check your .env file.");
+// }
+
+
+import { config } from './config';
+const ASGARDEO_CLIENT_ID = config.ASGARDEO_CLIENT_ID;
+const ASGARDEO_BASE_URL = config.ASGARDEO_BASE_URL;
+
+console.log(import.meta.env.VITE_ASGARDEO_CLIENT_ID) 
+console.log(import.meta.env.VITE_ASGARDEO_BASE_URL) 
 
 const queryClient = new QueryClient();
 
